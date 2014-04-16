c81b72d0535042605c727aa4b19a9046f7657cc1
silverp1 <jpettit@pettitservers.com>
2014-04-16 17:23:17 -0400

    Working on adding users model with session management

--- storm.py
+++ storm.py
@@ -1,14 +1,26 @@
 from flask import *
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 from forms import RegistrationForm
 
 import db
-from secret import HOSTNAME , USERNAME, PASSWORD, DATABASE
+from users import User
+from secret import HOSTNAME , USERNAME, PASSWORD, DATABASE, SESSION_KEY
 from register import makePassword
-
 
 app = Flask(__name__)
 con = db.getConnection(HOSTNAME,USERNAME,PASSWORD,DATABASE)
+
+app.secret_key = SESSION_KEY
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+db = SQLAlchemy(app)
+
+@login_manager.user_loader
+def load_user(id):
+	return User.query.get(int(id))
 
 @app.route('/')
 def index():
@@ -20,24 +32,26 @@ def login():
 
 @app.route('/register', methods=['POST','GET'])
 def register(): 	
-	if request.method == 'POST':
-	        form = RegistrationForm(request.form)
-		if form.validate():
-			hashedPassword = ""
-			username = ""
-			email = ""
-			hashedPassword = hashedPassword.join((makePassword(form.password.data)))
-			username = username.join(form.username.data)
-			email = email.join(form.email.data)
-			cur = con.cursor()
-			cur.execute("INSERT INTO users(username,email,password) VALUES('%s','%s','%s');" % (username, email, hashedPassword))
-			return redirect(url_for('login')) 
-		else:
-			return render_template('register.html',error="Please fill out the entire form",form=form)
-	else:
-		print "Request was not a POST"
 		form = RegistrationForm(request.form)
 		return render_template('register.html', form=form)
+
+@app.route('/register/do', methods=['POST','GET'])
+def registerDo():
+	if request.method == 'POST':
+                form = RegistrationForm(request.form)
+                if form.validate():
+			user = User(form.username.data, form.password.data, form.email.data)
+			db.session.add(user)
+			db.session.commit()
+			flash('User registered')	
+	                return redirect(url_for('login'))
+                else:
+                        return render_template('register.html',error="Please fill out the entire form",form=form)
+        else:
+                print "Request was not a POST"
+                form = RegistrationForm(request.form)
+                return render_template('register.html', form=form)
+
 
 @app.route('/account')
 def account():
